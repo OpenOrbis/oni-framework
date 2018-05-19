f38ce8b7e6cb006ce784996a9ee642cb502d87dd
balika011 <balika011@gmail.com>
2018-05-19 17:26:17 +0200

    add sys_dynlib_unload_prx

--- include/oni/utils/dynlib.h
+++ include/oni/utils/dynlib.h
@@ -2,5 +2,6 @@
 #include <oni/utils/types.h>
 
 int64_t sys_dynlib_load_prx(char* prxPath);
+int64_t sys_dynlib_unload_prx(int64_t prxID);
 int64_t sys_dynlib_dlsym(int64_t moduleHandle, const char* functionName, void *destFuncOffset);
 
--- src/oni/utils/dynlib.c
+++ src/oni/utils/dynlib.c
@@ -6,6 +6,11 @@ int64_t sys_dynlib_load_prx(char* prxPath)
 	return (int64_t)syscall1(594, prxPath);
 }
 
+int64_t sys_dynlib_unload_prx(int64_t prxID)
+{
+	return (int64_t)syscall1(595, (void*)prxID);
+}
+
 int64_t sys_dynlib_dlsym(int64_t moduleHandle, const char* functionName, void *destFuncOffset)
 {
 	return (int64_t)syscall3(591, (void*)moduleHandle, (void*)functionName, destFuncOffset);
