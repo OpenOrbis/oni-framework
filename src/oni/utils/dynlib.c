a76afc7bf9032d90d087513d137a39cdcea67b4a
balika011 <balika011>
2018-05-19 14:29:59 +0200

    add dynlib util

---
+++ src/oni/utils/dynlib.c
@@ -0,0 +1,12 @@
+#include <oni/utils/dynlib.h>
+#include <oni/utils/syscall.h>
+
+int64_t sys_dynlib_load_prx(char* prxPath)
+{
+	return (int64_t)syscall1(594, prxPath);
+}
+
+int64_t sys_dynlib_dlsym(int64_t moduleHandle, const char* functionName, void *destFuncOffset)
+{
+	return (int64_t)syscall3(591, (void*)moduleHandle, (void*)functionName, destFuncOffset);
+}
