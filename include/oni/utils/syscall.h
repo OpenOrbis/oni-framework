f1cff84712c060331277c9523b09d8907e410495
kiwidoggie <forums@allenthinks.com>
2018-11-17 20:10:16 -0800

    Add option for _Allocate3MB as well as _Allocate5MB

--- include/oni/utils/syscall.h
+++ include/oni/utils/syscall.h
@@ -38,4 +38,5 @@ void* syscall5(
 
 //caddr_t _mmap(caddr_t addr, size_t len, int prot, int flags, int fd, off_t pos);
 caddr_t _mmap(void);
+caddr_t _Allocate3MB(void);
 caddr_t _Allocate5MB(void);
--- src/oni/utils/memory/syscall.s
+++ src/oni/utils/memory/syscall.s
@@ -1,7 +1,7 @@
 .intel_syntax noprefix
 .text
 
-.global syscall1, syscall2, syscall3, syscall4, syscall5, _Allocate5MB, _mmap
+.global syscall1, syscall2, syscall3, syscall4, syscall5, _Allocate3MB, _Allocate5MB, _mmap
 
 syscall:
     mov rax,rdi
@@ -48,12 +48,23 @@ syscall5:
     syscall
     ret
 
+_Allocate3MB:
+	mov rax, 477
+	mov rdi, 0
+	mov rsi, 3145728 # Allocate 5MB # 0x4000 = 16384
+	mov rdx, 7 # PROT_READ | PROT_WRITE | PROT_EXEC
+	mov r10, 4097 # MAP_ANON | MAP_SHARED
+	mov r8, -1
+	mov r9, 0
+	syscall
+	ret
+
 _Allocate5MB:
 	mov rax, 477
 	mov rdi, 0
 	mov rsi, 5242880 # Allocate 5MB # 0x4000 = 16384
 	mov rdx, 7 # PROT_READ | PROT_WRITE | PROT_EXEC
-	mov r10, 4096 # MAP_ANON
+	mov r10, 4097 # MAP_ANON | MAP_SHARED
 	mov r8, -1
 	mov r9, 0
 	syscall
